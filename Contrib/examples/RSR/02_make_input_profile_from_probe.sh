cfb9edc4d0cfd0a5e02eec4e3ccc3c2d5b2afe14
jyoz <jyoz@088ae1b8-34b2-e24d-a5c0-8cc67dc321ae>
2007-07-29 15:42:24 +0000

    Use extracted edge length from probe passed back by extract_probe_data; use sips utility to extract the embedded profile from the grabbed probe image.
    git-svn-id: https://sampleicc.svn.sourceforge.net/svnroot/sampleicc/trunk@168 088ae1b8-34b2-e24d-a5c0-8cc67dc321ae

--- Contrib/examples/RSR/02_make_input_profile_from_probe.sh
+++ Contrib/examples/RSR/02_make_input_profile_from_probe.sh
@@ -3,7 +3,7 @@
 my_name=$0
 
 if [[ $# < 4 || $# > 5 ]]; then
-  echo "{$my_name:t} : usage is $my_name:t grabbed_probe_image.tiff profile_description copyright_holder profile_path.icc"
+  echo "{$my_name:t} : usage is $my_name:t grabbed_probe_image.tiff profile_description copyright_holder profile_path.icc [pretransform_path]"
   exit 1
 fi
 
@@ -12,36 +12,31 @@ profile_description=$2
 copyright_holder=$3
 profile_path=$4
 extracted_probe_values=/tmp/probe_extraction.$$.txt
-
-N=52
-CONTENT_LEFT=2
-CONTENT_RIGHT=565
-CONTENT_BOTTOM=2
-CONTENT_TOP=285
+extracted_monitor_profile=/tmp/extracted_monitor.$$.icc
 
 MONITOR_PROFILE_PATH="/var/tmp/embedded_profile_in_grabbed_image_here.icc"
 PRETRANSFORM_PATH=""
-if [[ $# == 4 ]]; then
-  PRETRANSFORM_PATH=$4
+if [[ $# == 5 ]]; then
+  PRETRANSFORM_PATH=$5
 fi
 
-extract_probe_data \
-    $grabbed_probe_image \
-    $extracted_probe_values \
-    $N $CONTENT_LEFT $CONTENT_RIGHT $CONTENT_BOTTOM $CONTENT_TOP
+N=`extract_probe_data $grabbed_probe_image $extracted_probe_values`
 extraction_status=$?
 if [ $extraction_status != 0 ] ; then
     echo "error : ${my_name:t} : nonzero exit code ($extraction_status) returned from extract_probe_data"
     exit -1
 fi
 
+sips --extractProfile $extracted_monitor_profile $grabbed_probe_image
+
 create_CLUT_profile_from_probe \
     $N \
     $extracted_probe_values \
-    $MONITOR_PROFILE_PATH \
+    $extracted_monitor_profile \
     "(probed) $profile_description" \
-    $copyright_holder \
-    $profile_path $PRETRANSFORM_PATH
+    $profile_path \
+    "$copyright_holder" \
+    $PRETRANSFORM_PATH
 creation_status=$?
 if [ $creation_status != 0 ] ; then
     echo "error : ${my_name:t} : nonzero exit code ($creation_status) returned from create_CLUT_profile_from_probe"
